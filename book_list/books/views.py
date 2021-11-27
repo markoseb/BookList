c82551531941fac2e65def2a63012d601d406515
Markos <marek.chlopek@protonmail.com>
2021-11-27 18:50:56 +0100

    Img link. Edit Book

--- book_list/books/views.py
+++ book_list/books/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, Blueprint
+from flask import render_template, url_for, flash, redirect, Blueprint, request
 from book_list import db
 from book_list.models import Book
 from book_list.books.forms import BookForm
@@ -22,7 +22,35 @@ def add_book():
 
         db.session.add(book)
         db.session.commit()
-        flash("Book Added")
+        flash("Dodano książkę!")
         return redirect(url_for('core.index'))
+
+    return render_template('addbook.html', form=form)
+
+
+@books.route('/book/<int:book_id>', methods=["GET", "POST"])
+def update(book_id):
+    # grab the requested blog post by id number or return 404
+    book = Book.query.get_or_404(book_id)
+    form = BookForm()
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.author = form.author.data
+        book.pub_date = form.pub_date.data
+        book.isbn = form.isbn.data
+        book.link = form.img.data
+        book.lan = form.lan.data
+        book.pages_number = form.pages_number.data
+        db.session.commit()
+        flash("Zmiana zatwierdzona!")
+        return redirect(url_for('core.index'))
+    elif request.method == 'GET':
+        form.title.data = book.title
+        form.author.data = book.author
+        form.pub_date.data = book.pub_date
+        form.isbn.data = book.isbn
+        form.img.data = book.link
+        form.lan.data = book.lan
+        form.pages_number.data = book.pages_number
 
     return render_template('addbook.html', form=form)
--- book_list/core/views.py
+++ book_list/core/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint, redirect
+from flask import render_template, Blueprint, redirect, request
+from book_list.models import Book
 
 core = Blueprint('core', __name__)
 
@@ -14,4 +15,6 @@ def index():
     This is the home page view. Use pagination to show
     a Book List.
     '''
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.order_by(Book.id).paginate(page=page, per_page=100)
+    return render_template('index.html', books=books)
